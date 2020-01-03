7517ac2e5635bb5f793a31329e67835ed0b1d0c6
Daniel <dan1510123@gmail.com>
2020-01-03 17:11:08 +0800

    Initial Commit

---
+++ MinutesLeft WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MinutesLeft WatchKit Extension
+//
+//  Created by Daniel Luo on 1/3/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MinutesLeft WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  MinutesLeft WatchKit Extension
+//
+//  Created by Daniel Luo on 1/3/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
